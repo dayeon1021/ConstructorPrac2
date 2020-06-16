6292bd5fe4f3f1fd34324cf09d7e2f1f24b5fa80
dayeon1021 <s2018s20@e-mirim.hs.kr>
2020-06-16 14:02:21 +0900

    초기 커밋.

---
+++ ConstructorPrac2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorPrac2
+{
+    class Program
+    {
+        class Parent{
+            public Parent()
+            {
+                Console.WriteLine("부모 생성자 호출");
+            }
+        }
+        class Child : Parent
+        {
+            public Child()
+            {
+                Console.WriteLine("자식 생성자 호출");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Child c = new Child();
+        }
+    }
+}
